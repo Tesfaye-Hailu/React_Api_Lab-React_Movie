93076dbfd2864de94c1d37612692bb0133fab8d5
Tesfaye Hailu <tes.girmah@gmail.com>
2023-05-21 17:13:05 -0700

    react movies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MovieDisplay from "./Components/MovieDisplay";
-import Form from "./Components/Form";
+import MovieDisplay from './Components/MovieDisplay';
+import Form from './Components/Form';
 
 function App() {
+  const apiKey = '98e3fb1f';
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    const data = await response.json();
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    getMovie('Titanic');
+  }, []);
+
   return (
     <div className="App">
-     <Form />
-      <MovieDisplay />     
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
 
 export default App;
-
-
